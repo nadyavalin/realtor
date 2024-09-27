883d63bf73cd2b1eb4570ca7d1bd83e4263523cd
nadyavalin <nadejda.sergeevna.tkachuk@gmail.com>
2024-09-27 10:55:27 +0300

    chore: config eslint and prettier

--- assets/scripts/stickyHeader.js
+++
@@ -1,9 +0,0 @@
-const header = document.querySelector(".header");
-
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 50) {
-    header.classList.add("shrink");
-  } else {
-    header.classList.remove("shrink");
-  }
-});
--- index.js
+++ index.js
@@ -1,3 +1,61 @@
+/* Sticky header */
+const header = document.querySelector(".header");
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    header.classList.add("shrink");
+  } else {
+    header.classList.remove("shrink");
+  }
+});
+
+/* Smooth scroll to blocks */
+function scrollToElement(target) {
+  const targetElement = document.querySelector(target);
+  if (targetElement) {
+    const topPosition =
+      targetElement.getBoundingClientRect().top + window.scrollY - 80;
+    window.scrollTo({
+      top: topPosition,
+      behavior: "smooth",
+    });
+
+    history.pushState(null, "", target);
+  }
+}
+
+/* Burger menu */
+const burgerMenu = document.querySelector(".burger-menu");
+const navMenu = document.querySelector(".nav");
+const ulItem = document.querySelector(".navbar");
+
+burgerMenu.addEventListener("click", () => {
+  burgerMenu.classList.toggle("active");
+  navMenu.classList.toggle("open");
+});
+
+ulItem.addEventListener("click", () => {
+  navMenu.classList.remove("open");
+  burgerMenu.classList.remove("active");
+});
+
+/* Bank delay animation */
+const handleIntersection = (entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("animate");
+    }
+  });
+};
+
+const observer = new IntersectionObserver(handleIntersection);
+
+const targetElements = document.querySelectorAll(".bank");
+
+targetElements.forEach((element) => {
+  observer.observe(element);
+});
+
+/* Custom Select*/
 document.addEventListener("DOMContentLoaded", function () {
   const selectElements = document.querySelectorAll(".new-select-element");
 
